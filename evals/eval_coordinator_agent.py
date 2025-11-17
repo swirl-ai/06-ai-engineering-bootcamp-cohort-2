83a165cf5eccb4c4c7646f704f2be82128d2b644
aurimasgriciunas <griciunas.aurimas@gmail.com>
2025-11-17 02:30:44 +0200

    GitHub actions pipeline

--- evals/eval_coordinator_agent.py
+++ evals/eval_coordinator_agent.py
@@ -2,6 +2,11 @@ from langsmith import Client
 
 from api.agent.agents import coordinator_agent
 from api.agent.graph import State
+from time import sleep
+
+
+ACC_THRESHOLD = 0.6
+SLEEP_TIME = 5
 
 client = Client()
 
@@ -23,3 +28,28 @@ results = client.evaluate(
     ],
     experiment_prefix="coordinator-eval-dataset"
 )
+
+
+print(f"Sleeping for {SLEEP_TIME} seconds...")
+sleep(SLEEP_TIME)
+
+
+results_resp = client.read_project(
+    project_name = results.experiment_name,
+    include_stats = True
+)
+
+avg_metric = results_resp.feedback_stats.get("next_agent_evaluator").get("avg")
+errors = results_resp.feedback_stats.get("next_agent_evaluator").get("errors")
+
+if avg_metric >= ACC_THRESHOLD:
+    output_messge = f"✅ Success: {avg_metric}"
+else:
+    output_messge = f"❌ Failure: {avg_metric}"
+
+if errors > 0:
+    raise AssertionError(f"There were {errors} errors while running evaluations")
+elif avg_metric >= ACC_THRESHOLD:
+    print(output_messge, flush=True)
+else:
+    raise AssertionError(output_messge)
